d4a7d0b403820c3167e9566a0978a5dd9b4394d0
Archita Saraswati <architasaraswati@gmail.com>
2025-06-22 20:48:18 +0530

    first commit

---
+++ data/testimonial.js
@@ -0,0 +1,47 @@
+export const testimonial = [
+
+{
+  quote: "The AI feedback on mock interviews was incredibly accurate. I walked into my interviews fully prepared.",
+  author: "Liam Anderson",
+  image: "https://randomuser.me/api/portraits/men/44.jpg",
+  role: "Data Analyst",
+  company: "BrightMetrics",
+},
+{
+  quote: "I’ve tried other platforms, but this was different—smarter, more personalized, and honestly, more effective.",
+  author: "Sophia Martinez",
+  image: "https://randomuser.me/api/portraits/women/32.jpg",
+  role: "Marketing Associate",
+  company: "BrandHive",
+},
+{
+  quote: "AI-powered suggestions made my resume go from average to standout. I got 3 callbacks in a week!",
+  author: "Noah Thompson",
+  image: "https://randomuser.me/api/portraits/men/34.jpg",
+  role: "Software Developer",
+  company: "Skybyte Solutions",
+},
+{
+  quote: "From mock interviews to job role insights, everything felt like it was built just for me.",
+  author: "Ava Robinson",
+  image: "https://randomuser.me/api/portraits/women/49.jpg",
+  role: "Business Analyst",
+  company: "NextCore Consulting",
+},
+{
+  quote: "What stood out was how precise and industry-specific the interview prep was. Totally worth it.",
+  author: "Ethan Walker",
+  image: "https://randomuser.me/api/portraits/men/75.jpg",
+  role: "DevOps Engineer",
+  company: "CloudForge Tech",
+},
+{
+  quote: "This platform helped me revamp my resume and land interviews at top U.S. startups. Super intuitive and powerful!",
+  author: "Emily Carter",
+  image: "https://randomuser.me/api/portraits/women/58.jpg",
+  role: "UX Designer",
+  company: "Designly Inc.",
+},
+
+
+];
